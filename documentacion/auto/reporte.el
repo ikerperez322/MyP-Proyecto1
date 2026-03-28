01c26811acdec89593316ba5ba948b07c7ba0364
Iker Pérez <iker.perez@ciencias.unam.mx>
2026-03-27 23:25:58 -0600

    Agregando documentación

---
+++ documentacion/auto/reporte.el
@@ -0,0 +1,33 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "reporte"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "spanish") ("inputenc" "utf8") ("fontenc" "T1") ("geometry" "") ("graphicx" "") ("listings" "") ("xcolor" "") ("hyperref" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "babel"
+    "inputenc"
+    "fontenc"
+    "geometry"
+    "graphicx"
+    "listings"
+    "xcolor"
+    "hyperref"))
+ :latex)
+
